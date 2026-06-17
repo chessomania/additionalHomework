bf578f4c4bb552c7ac634ada0d1b492153f6c5cc
Robert Ghazaryan <chessomania@outlook.com>
2026-06-17 18:43:51 +0400

    made all tasks with different functions, added 4 task

--- TrianglesWithInput.java
+++ TrianglesWithInput.java
@@ -5,6 +5,58 @@ public class TrianglesWithInput {
     {
         return height + height - 1;
     }
+    public static void triangleLeftToRight(int height)
+    {
+        for (int i = 0; i <= height; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void triangleRightToLeft(int height)
+    {
+        for (int i = 0; i <= height; i++)
+        {
+            for (int j = height; j > i; j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void centeredTriangleUp(int height)
+    {
+        int rectangleHeight = rectHeight(height); // finding height where position "height" will be centered
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j <= rectangleHeight; j++)
+            {
+                if(j <= height + i && j >= height - i)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void centeredTriangleBottom(int height)
+    {
+        int rectangleHeight = rectHeight(height); // finding height where position "height" will be centered
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j <= rectangleHeight; j++)
+            {
+                if(j > i && j <= rectangleHeight - i)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner1 = new Scanner(System.in);
 
@@ -16,41 +68,22 @@ public class TrianglesWithInput {
         }
         //first task
         System.out.println("First Task!");
-        for (int i = 0; i <= height; i++)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
+        triangleLeftToRight(height);
         //second task
         System.out.print("\n");
         System.out.println("Second Task!");
         System.out.print("\n");
-        for (int i = 0; i <= height; i++)
-        {
-            for (int j = height; j > i; j--)
-            {
-                System.out.print("*");
-            }
-            System.out.print("\n");
-        }
-        //second task
+        triangleRightToLeft(height);
+        //third task
         System.out.print("\n");
         System.out.println("Third Task!");
         System.out.print("\n");
-        int rectangleHeight = rectHeight(height); // finding height where position "height" will be centred
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j <= rectangleHeight; j++)
-            {
-                if(j <= height + i && j >= height - i)
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
-            }
-            System.out.print("\n");
-        }
+        centeredTriangleUp(height);
+        //fourth task
+        System.out.print("\n");
+        System.out.println("fourth Task!");
+        System.out.print("\n");
+        centeredTriangleUp(height);
+        centeredTriangleBottom(height);
     }
 }
